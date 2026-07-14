1bb65d869af69c4463ec757118114cdfff39334b
Chaitanya Liz <liz362002@gmail.com>
2026-07-14 20:13:58 +0530

    add report to blob storage

--- scripts/detect-drift.py
+++ scripts/detect-drift.py
@@ -1,5 +1,10 @@
 import json
+import os
+from dotenv import load_dotenv
+from datetime import datetime, UTC
+from azure.storage.blob import BlobServiceClient
 
+# Load Terraform plan
 with open("reports/plan.json", "r") as f:
     plan = json.load(f)
 
@@ -18,7 +23,11 @@ def find_changes(before, after, path=""):
         for key in set(before) | set(after):
             new_path = f"{path}.{key}" if path else key
             changes.extend(
-                find_changes(before.get(key), after.get(key), new_path)
+                find_changes(
+                    before.get(key),
+                    after.get(key),
+                    new_path
+                )
             )
 
     elif before != after and not is_trivial_change(before, after):
@@ -31,6 +40,7 @@ def find_changes(before, after, path=""):
     return changes
 
 
+# Detect drift
 report = []
 
 for drift in plan.get("resource_drift", []):
@@ -45,5 +55,57 @@ for drift in plan.get("resource_drift", []):
             "changes": changes
         })
 
-with open("reports/drift_report.json", "w") as f:
+# Exit if no drift found
+if not report:
+    print("No drift detected. No report generated.")
+    exit(0)
+
+# Generate report
+report_file = "reports/drift_report.json"
+
+with open(report_file, "w") as f:
     json.dump(report, f, indent=4)
+
+print(f"Drift detected. Report generated: {report_file}")
+
+# -------------------------------
+# Upload report to Blob Storage
+# -------------------------------
+
+load_dotenv()
+connection_string = os.getenv(
+    "ACCESS_TOKEN_STORAGE_ACC"
+)
+
+if not connection_string:
+    raise ValueError(
+        "ACCESS_TOKEN_STORAGE_ACC environment variable not found"
+    )
+
+container_name = "drift-reports"
+
+blob_service_client = BlobServiceClient.from_connection_string(
+    connection_string
+)
+
+blob_name = (
+    f"drift_report_"
+    f"{datetime.now(UTC).strftime('%Y%m%d_%H%M%S')}.json"
+)
+
+blob_client = blob_service_client.get_blob_client(
+    container=container_name,
+    blob=blob_name
+)
+
+with open(report_file, "rb") as data:
+    blob_client.upload_blob(
+        data,
+        overwrite=False
+    )
+
+print(
+    f"Report uploaded successfully to "
+    f"container '{container_name}' "
+    f"as '{blob_name}'"
+)
